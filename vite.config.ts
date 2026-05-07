0b3e950ebfe1552987e444f72ee8d7df01608fdc
Federico Salgado <federico.salgado04@gmail.com>
2026-05-07 15:44:56 +0000

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portafolio/",
+  base: "https://f3d9.github.io/",
   plugins: [
     react(),
     tailwindcss(),
