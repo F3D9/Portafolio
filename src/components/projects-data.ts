5a1c5248da789aeaa487e2539e2236e212709be2
Federico Salgado <federico.salgado04@gmail.com>
2026-08-03 20:38:30 +0000

    actualizo el portafolio

--- src/components/projects-data.ts
+++ src/components/projects-data.ts
@@ -1,7 +1,10 @@
 import preview from "../assets/chatbot5.png";
 import preview2 from "../assets/chatbot2.png";
 import preview3 from "../assets/chatbot3.png";
-import GymTracker from "../assets/GymTracker.png";
+import gymtracker1 from "../assets/gymtracker1.png";
+import gymtracker2 from "../assets/gymtracker2.png";
+import gymtracker3 from "../assets/gymtracker3.png";
+import gymtracker4 from "../assets/gymtracker4.png";
 
 export interface Project {
   title: string;
@@ -15,6 +18,16 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: "Gym Tracker",
+    status: "En Produccion",
+    description:
+      "Aplicación para registrar rutinas de entrenamiento, con backend en NestJS y frontend en React. Utiliza PostgreSQL para almacenamiento de datos y Prisma como ORM. Desplegado en Render para el Backend y Github Pages para el Frontend. Incluye autenticación de usuarios y manejo de sesiones.",
+    tags: ["Nestjs", "TypeScript","PostgreSQL", "React","Vite","Prisma","Docker"],
+    images: [gymtracker1, gymtracker2, gymtracker3, gymtracker4],
+    github: "https://github.com/F3D9/Gym-Tracker-Frontend",
+    demo: "https://f3d9.github.io/Gym-Tracker-Frontend/",
+  },
+  {
     title: "AI Chatbot",
     status: "En producción",
     description:
@@ -24,16 +37,7 @@ export const projects: Project[] = [
     github: "https://github.com/F3D9/ChatBotNodeJs",
     demo: "https://chatbotnodejs.up.railway.app",
   },
-  {
-    title: "Gym Tracker",
-    status: "En desarollo",
-    description:
-      "Aplicación para registrar rutinas de entrenamiento, con backend en NestJS y frontend en React. Utiliza PostgreSQL para almacenamiento de datos y Prisma como ORM. Desplegado con Docker. Todavia no hay demo disponible",
-    tags: ["Nestjs", "TypeScript","PostgreSQL", "React","Vite","Prisma","Dcker"],
-    images: [GymTracker],
-    github: "",
-    demo: "",
-  },
+  
   // agregá más proyectos acá con la misma estructura
   
 ];
